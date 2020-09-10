feda2239a5bab49212717271b74c11d48df9a2aa
whew <51290160+whew@users.noreply.github.com>
2020-09-10 11:34:09 +0000

    Add files via upload

--- manyame.bat
+++ manyame.bat
@@ -1,3 +1,10 @@
 @echo off
-bash manyame.sh %*
+setlocal EnableExtensions
+
+rem get unique file name 
+:uniqLoop
+set "uniqueFileName=%tmp%\bat~%RANDOM%.tmp"
+if exist "%uniqueFileName%" goto :uniqLoop
+bash manyame.sh "%uniqueFileName%" %*
+echo Success! Press enter to exit...
 pause >nul
--- manyame.sh
+++ manyame.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 mkdir -p Downloads
+tmp="$1"
 echo -n "Enter Title: " 
 read title
 echo -n "Enter Episode: " 
 read episode
 chmonimeperc=$(echo "$title" | sed 's/ /%20/g')
-botlist=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq)
+#botlist=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq)
 animelist=$(curl -s "https://api.nibl.co.uk/nibl/search?query=$chmonimeperc&episodeNumber=$episode" | jq)
-choose=$(echo "$animelist" | jq -r ".content[] .name" | sort | uniq | fzf | sed 's/\[/./g;s/\]/./g')
+choose=$(echo "$animelist" | jq -r ".content[] .name" | sort | uniq | fzf -m --reverse | sed 's/\[/./g;s/\]/./g')
+echo "$choose" > "$tmp"
 botnumber=$(echo "$animelist" | grep -B2 "$choose" | head -n1 | grep -o -E '[0-9]+')
 botname=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq -r '.content[] | "\(.id) \(.name)"' | grep "^$botnumber" | awk '{print $2}' | head -n1)
-pacname=$(echo "$animelist" | grep -B1 "$choose" | head -n1 | grep -o -E '[0-9]+')
-xdccget -d Downloads -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
+while IFS= read -r line ; do
+	pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
+	xdccget -d Downloads -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
+done < "$tmp"
