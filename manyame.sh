8087c32e7208152845ef18dc9cf44d8c463b840e
whew <51290160+whew@users.noreply.github.com>
2020-09-11 18:12:33 +0000

    Update manyame.sh

--- manyame.sh
+++ manyame.sh
@@ -7,18 +7,24 @@ read episode
 chmonimeperc=$(echo "$title" | sed 's/ /%20/g')
 #botlist=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq)
 animelist=$(curl -s "https://api.nibl.co.uk/nibl/search?query=$chmonimeperc&episodeNumber=$episode" | jq)
-choose=$(echo "$animelist" | jq -r ".content[] .name" | sort | uniq | fzf -m --reverse | sed 's/\[/./g;s/\]/./g')
-echo "$choose" > "$1"
+choose=$(echo "$animelist" | jq -r '.content[] | .size + " | " + .name' | sort | uniq | fzf -m --reverse | sed 's/\[/./g;s/\]/./g')
+choose=$(echo "$choose" | sed 's/^.*| //')
+if uname | grep -q "Windows" ; then
+    echo "$choose" > "$1"
+else
+    true
+fi
 botnumber=$(echo "$animelist" | grep -B2 "$choose" | head -n1 | grep -o -E '[0-9]+')
 botname=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq -r '.content[] | "\(.id) \(.name)"' | grep "^$botnumber" | awk '{print $2}' | head -n1)
 if uname | grep -q "Windows" ; then
-	while IFS= read -r line ; do
-		pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
-		echo "xdccget.exe --dont-confirm-offsets -d Downloads -i \"irc.rizon.net\" \"#nibl\" \"$botname xdcc send #$pacname\"" >> "$2"
-	done < "$1" 
+    while IFS= read -r line ; do
+        pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
+        echo "xdccget.exe --dont-confirm-offsets -d Downloads -q \"irc.rizon.net\" \"#nibl\" \"$botname xdcc send #$pacname\"" >> "$2"
+    done < "$1" 
 else
-	echo "$choose" | while IFS= read -r line ; do
-		pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
-		xdccget -d Downloads -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
-	done
+    while IFS= read -r line ; do
+        pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
+        xdccget -d Downloads -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
+    done < "$choose" 
 fi
+
