59e61260f0755261062504ef7dc008b9ade3cef8
whew <51290160+whew@users.noreply.github.com>
2020-09-18 16:27:42 +0000

    auto creating folder with anime title

--- manyame.sh
+++ manyame.sh
@@ -9,6 +9,13 @@ else
 	echo "Don't forget slash at the end!"
 	echo -n ''
 	read -r animefolder
+	if echo "$animefolder" | grep -v '\\$\|/$' ; then
+		echo "Please, don't forget slash at the end!"
+		echo ""
+		echo "Press enter to exit"
+		read key
+		exit
+	fi
 	echo "f $animefolder" >> manyame.conf
 fi
 folder=$(cat manyame.conf | grep ^f | awk '{$1=""; print $0}')
@@ -19,7 +26,7 @@ read -r episode
 chmonimeperc=$(echo "$title" | sed 's/ /%20/g')
 #botlist=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq)
 animelist=$(curl -s "https://api.nibl.co.uk/nibl/search?query=$chmonimeperc&episodeNumber=$episode" | jq '.')
-choose=$(echo "$animelist" | jq -r '.content[] | .size + " | " + .name' | sort | uniq | fzf -m --reverse | sed 's/\[/./g;s/\]/./g')
+choose=$(echo "$animelist" | jq -r '.content[] | .size + " | " + .name' | sort | uniq | fzf -m --reverse)
 choose=$(echo "$choose" | sed 's/^.*| //')
 if uname | grep -i -q "Windows\|Mingw\|Cygwin" ; then
     echo "$choose" > "$1"
@@ -30,13 +37,21 @@ botnumber=$(echo "$animelist" | grep -B2 "$choose" | head -n1 | grep -o -E '[0-9
 botname=$(curl -s "https://api.nibl.co.uk/nibl/bots" | jq -r '.content[] | "\(.id) \(.name)"' | grep "^$botnumber" | awk '{print $2}' | head -n1)
 if uname | grep -i -q "Windows\|Mingw\|Cygwin" ; then
     while IFS= read -r line ; do
-        pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
-        echo "xdccget.exe --dont-confirm-offsets -d $folder -q \"irc.rizon.net\" \"#nibl\" \"$botname xdcc send #$pacname\"" >> "$2"
+	anime=$(echo "$line" |  sed 's/\[/\\\[/g;s/\]/\\\]/g')
+        pacname=$(echo "$animelist" | grep -B1 "$anime" | head -n1 | grep -o -E '[0-9]+')
+	dirname=$(echo "$line" | grep "\[.*\]" | grep "\-" | sed -e 's/\[[^][]*\]//g;s/^ //' | awk -F"-" '{print $NR}')
+	foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//')
+	echo "mkdir \"$foldir\"" >> "$2"
+        echo "xdccget.exe --dont-confirm-offsets -d \"$foldir\" -q \"irc.rizon.net\" \"#nibl\" \"$botname xdcc send #$pacname\"" >> "$2"
     done < "$1"
 else
     echo "$choose" | while IFS= read -r line ; do
-        pacname=$(echo "$animelist" | grep -B1 "$line" | head -n1 | grep -o -E '[0-9]+')
-        xdccget -d $folder -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
+    	anime=$(echo "$line" | sed 's/\[/\\\[/g;s/\]/\\\]/g')
+        pacname=$(echo "$animelist" | grep -B1 "$anime" | head -n1 | grep -o -E '[0-9]+')
+	dirname=$(echo "$line" | grep "\[.*\]" | grep "\-" | sed -e 's/\[[^][]*\]//g;s/^ //' | awk -F"-" '{print $NR}')
+	foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//')
+	mkdir -p "$foldir"
+        xdccget -d $foldir -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
     done
 fi
 
