72851cdc203ce32cf2c1d2d3a848b4ef1091c68e
whew <51290160+whew@users.noreply.github.com>
2020-09-18 23:53:51 +0000

    Update manyame.bat

--- manyame.bat
+++ manyame.bat
@@ -12,7 +12,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (
 `curl -sI https://raw.githubusercontent.com/whew/manyame/master/manyame.sh  ^| awk "/Content-Length/ { print $2 }"`) DO (
 SET remote=%%F
 )
-IF !remote! EQU !local! (GOTO dl) ELSE (GOTO uniqLoop)
+IF %remote% EQU %local% (GOTO uniqLoop) ELSE (GOTO dl)
 :dl
 curl -sLO https://raw.githubusercontent.com/whew/manyame/master/manyame.sh
 GOTO uniqLoop
