5a7a9cb4a929be88373ee53fd4cbe308b5cb8fbd
whew <51290160+whew@users.noreply.github.com>
2020-09-18 21:33:01 +0000

    added autoupdate

--- manyame.bat
+++ manyame.bat
@@ -1,5 +1,20 @@
 @echo off
 setlocal EnableExtensions
+for %%I in (manyame.sh) do @echo %%~zI
+curl -sI https://raw.githubusercontent.com/whew/manyame/master/manyame.sh  | awk "/Content-Length/ { print $2 }"
+for /f "tokens=* USEBACKQ" %%a in (
+`for %%I in ^(manyame.sh^) do @echo %%~zI`
+) do (
+set local=%%a
+)
+FOR /F "tokens=* USEBACKQ" %%F IN (
+`curl -sI https://raw.githubusercontent.com/whew/manyame/master/manyame.sh  ^| awk "/Content-Length/ { print $2 }"`) DO (
+SET remote=%%F
+)
+IF !remote! EQU !local! (GOTO dl) ELSE (GOTO uniqLoop)
+:dl
+curl -sLO https://raw.githubusercontent.com/whew/manyame/master/manyame.sh
+GOTO uniqLoop
 :uniqLoop
 set "uniqueFileName=%tmp%\rand%RANDOM%.tmp"
 if exist "%uniqueFileName%" goto :uniqLoop
