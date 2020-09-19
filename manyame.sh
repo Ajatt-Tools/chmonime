55da23cfbae7f499c893555ec6872ebfca87f4f0
whew <51290160+whew@users.noreply.github.com>
2020-09-19 08:46:24 +0000

    updated anime detection and folder namings

--- manyame.sh
+++ manyame.sh
@@ -33,24 +33,27 @@ if uname | grep -i -q "Windows\|Mingw\|Cygwin" ; then
 else
     true
 fi
-dirname=$(curl -s "https://kitsu.io/api/edge/anime?filter\[text\]=$chmonimeperc&page\[limit\]=1" | ./jq -r .data[].attributes.canonicalTitle)
 if uname | grep -i -q "Windows\|Mingw\|Cygwin" ; then
     while IFS= read -r line ; do
         anime=$(echo "$line" |  sed 's/\[/\\\[/g;s/\]/\\\]/g')
+        nosquare=$(echo "$anime" | sed 's/\[[^]]*\]//g;s/([^)]*)//g;s/\.[^.]*$//;s/^ *//g;s/ *$//;s/ /%20/g')
+        dirname=$(curl -s "https://kitsu.io/api/edge/anime?filter\[text\]=$nosquare&page\[limit\]=1" | ./jq -r .data[].attributes.canonicalTitle)
         botnumber=$(echo "$animelist" | grep -B2 "$anime" | head -n1 | grep -o -E '[0-9]+')
         botname=$(echo "$botlist" | grep "^$botnumber" | awk '{print $2}' | head -n1)
         pacname=$(echo "$animelist" | grep -B1 "$anime" | head -n1 | grep -o -E '[0-9]+')
-        foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//')
+        foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//;s/\/$//;s/\\//')
         echo "if not exist \"$foldir\" mkdir \"$foldir\" > nul 2> nul" >> "$2"
         echo "xdccget.exe --dont-confirm-offsets -d \"$foldir\" -q \"irc.rizon.net\" \"#nibl\" \"$botname xdcc send #$pacname\"" >> "$2"
     done < "$1"
 else
     echo "$choose" | while IFS= read -r line ; do
         anime=$(echo "$line" | sed 's/\[/\\\[/g;s/\]/\\\]/g')
+        nosquare=$(echo "$anime" | sed 's/\[[^]]*\]//g;s/([^)]*)//g;s/\.[^.]*$//;s/^ *//g;s/ *$//;s/ /%20/g')
+        dirname=$(curl -s "https://kitsu.io/api/edge/anime?filter\[text\]=$nosquare&page\[limit\]=1" | ./jq -r .data[].attributes.canonicalTitle)
         botnumber=$(echo "$animelist" | grep -B2 "$anime" | head -n1 | grep -o -E '[0-9]+')
         botname=$(echo "$botlist" | grep "^$botnumber" | awk '{print $2}' | head -n1)
         pacname=$(echo "$animelist" | grep -B1 "$anime" | head -n1 | grep -o -E '[0-9]+')
-        foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//')
+        foldir=$(echo "$folder$dirname" | sed 's/^ //;s/ $//;s/\/$//')
         mkdir -p "$foldir"
         xdccget -d "$foldir" -q "irc.rizon.net" "#nibl" "$botname xdcc send #$pacname"
     done
