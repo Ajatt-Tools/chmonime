9dd87642626f3174aa0e5eea06e2d5394965f3b8
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-09-30 22:35:31 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,21 +1,21 @@
 @echo off
 setlocal EnableExtensions
 mode con: cols=100 lines=30
-if exist "manyame.sh" goto :check
-Executables\busybox wget -q https://raw.githubusercontent.com/Ajatt-Tools/manyame/master/manyame.sh -O manyame.sh
+if exist "chmonime.sh" goto :check
+Executables\busybox wget -q https://raw.githubusercontent.com/`ls/chmonime/master/chmonime.sh -O chmonime.sh
 :check
 for /f "tokens=* USEBACKQ" %%a in (
-`for %%I in ^(manyame.sh^) do @echo %%~zI`
+`for %%I in ^(chmonime.sh^) do @echo %%~zI`
 ) do (
 set local=%%a
 )
 FOR /F "tokens=* USEBACKQ" %%F IN (
-`Executables\busybox wget -q --spider --server-response https://raw.githubusercontent.com/Ajatt-Tools/manyame/master/manyame.sh -O - 2^>^&1 ^| Executables\busybox sed -ne "/Content-Length/{s/.*: //;p}"`) DO (
+`Executables\busybox wget -q --spider --server-response https://raw.githubusercontent.com/Ajatt-Tools/chmonime/master/chmonime.sh -O - 2^>^&1 ^| Executables\busybox sed -ne "/Content-Length/{s/.*: //;p}"`) DO (
 SET remote=%%F
 )
 IF %remote% EQU %local% (GOTO uniqLoop) ELSE (GOTO dl)
 :dl
-Executables\busybox wget -q https://raw.githubusercontent.com/Ajatt-Tools/manyame/master/manyame.sh -O manyame.sh
+Executables\busybox wget -q https://raw.githubusercontent.com/Ajatt-Tools/chmonime/master/chmonime.sh -O chmonime.sh
 GOTO uniqLoop
 :uniqLoop
 set "uniqueFileName=%tmp%\rand%RANDOM%.tmp"
@@ -24,7 +24,7 @@ if exist "%uniqueFileName%" goto :uniqLoop
 set "uniqueBatName=%tmp%\bat%RANDOM%.bat"
 if exist "%uniqueBatName%" goto :uniqBat
 echo cd /d %cd%\Executables > %uniqueBatName%
-Executables\busybox bash manyame.sh "%uniqueFileName%" "%uniqueBatName%" %*
+Executables\busybox bash chmonime.sh "%uniqueFileName%" "%uniqueBatName%" %*
 call %uniqueBatName%
 echo Success! Press enter to exit...
 pause >nul
